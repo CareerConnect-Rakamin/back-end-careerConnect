b0d6e6d1edc797ad33fd75c65f20db031e89c149
Junatius <cjuniar0@gmail.com>
2023-11-11 04:17:29 +0800

    feat: add app for running the application

--- src/app.js
+++ src/app.js
@@ -9,8 +9,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use(`/${api}`)
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
 })
