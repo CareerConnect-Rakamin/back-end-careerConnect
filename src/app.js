ccbdb5bad578b8d2d8a4de6482d2efbf81330d09
Junatius <cjuniar0@gmail.com>
2023-11-11 04:13:35 +0800

    feat: add app.js for application

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { api, port } = require('./config')
+const swaggerUi = require('swagger-ui-express')
+const specs = require('./utils/swagger')
+
+const app = express()
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.use(`/${api}`)
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
