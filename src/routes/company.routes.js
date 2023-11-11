a034e317e32e448b15d2d36c3bd61c8fff1b7c53
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-11 15:27:06 +0700

    feat: create company routes updateCompanyById

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -3,5 +3,6 @@ const { companyControllers } = require('../controllers')
 const router = require('express').Router()
 
 router.get('/:id', companyControllers.getCompanyById)
+router.put('/:id', companyControllers.updateCompanyById)
 
 module.exports = router
