f6422dd1af82424d9ffefcdf6cd74f31eeb9e31e
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-11 21:40:49 +0700

    feat:add middleware validator in routes company

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,9 +1,19 @@
 const { companyControllers } = require('../controllers')
 const { authMiddlewares } = require('../middlewares')
 const router = require('express').Router()
+const { validator } = require('../middlewares')
 
+const { validate, requirements } = validator
 router.use(authMiddlewares)
-router.get('/:id', companyControllers.getCompanyById)
-router.put('/:id', companyControllers.updateCompanyById)
+router.get(
+  '/:id',
+  [validate(requirements.getCompanyById)],
+  companyControllers.getCompanyById,
+)
+router.put(
+  '/:id',
+  [validate(requirements.updateCompanyById)],
+  companyControllers.updateCompanyById,
+)
 
 module.exports = router
