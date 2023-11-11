1da8536328ae5fef81451c87c3e1b52121712d12
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-11 22:38:15 +0700

    feat: add app.us(router) from ./routes

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const { api, port } = require('./config');
 const swaggerUi = require('swagger-ui-express');
 const specs = require('./utils/swagger');
-
+const router = require('./routes');
 const app = express();
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(router);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+module.exports = app;
