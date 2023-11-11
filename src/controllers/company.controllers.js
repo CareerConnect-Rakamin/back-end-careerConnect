83dd5e5568c8a37bc040f1ed7567c00c98f631d8
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-11 18:01:00 +0700

    fix: response error for company controllers

--- src/controllers/company.controllers.js
+++ src/controllers/company.controllers.js
@@ -4,11 +4,11 @@ const getCompanyById = async (req, res) => {
     const id = req.params.id
     const result = await companyServices.getCompanyById(id)
     res.json({
-      status: 'success',
+      message: 'Success',
       data: result,
     })
   } catch (err) {
-    res.status(err.status).json(err.message)
+    res.status(err.status).json({ message: err.message })
   }
 }
 
@@ -16,11 +16,11 @@ const updateCompanyById = async (req, res) => {
   try {
     const result = await companyServices.updateCompanyById(req.params, req.body)
     res.json({
-      status: 'success',
+      message: 'Success',
       data: result,
     })
   } catch (err) {
-    res.status(err.status).json(err.message)
+    res.status(err.status).json({ message: err.message })
   }
 }
 module.exports = { getCompanyById, updateCompanyById }
