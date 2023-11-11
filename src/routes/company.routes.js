e88d12a20ff016a1312516989214f475b00d81de
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-11 17:14:03 +0700

    feat: add authMiddleware for company routes

--- src/routes/company.routes.js
+++ src/routes/company.routes.js
@@ -1,7 +1,8 @@
 const { companyControllers } = require('../controllers')
-
+const { authMiddlewares } = require('../middlewares')
 const router = require('express').Router()
 
+router.use(authMiddlewares)
 router.get('/:id', companyControllers.getCompanyById)
 router.put('/:id', companyControllers.updateCompanyById)
 
