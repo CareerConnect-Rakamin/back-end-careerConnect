5bfcdd95696e9ecdf5df0d8ef41bc0427736de37
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-11 15:09:18 +0700

    feat: create repositories updateCompanyById

--- src/repositories/company.repositories.js
+++ src/repositories/company.repositories.js
@@ -1,9 +1,41 @@
 const { CompanyModel } = require('../models')
 
-const getCompanyById = async ({ params }) => {
-  const id = parseInt(params)
+const getCompanyById = async (id) => {
   const result = await CompanyModel.findByPk(id)
   return result
 }
 
-module.exports = { getCompanyById }
+const updateCompnyById = async (
+  id,
+  {
+    photo_profile,
+    name,
+    type,
+    description,
+    website,
+    email,
+    phone_number,
+    address,
+  },
+) => {
+  const result = await CompanyModel.update(
+    {
+      photo_profile,
+      name,
+      type,
+      description,
+      website,
+      email,
+      phone_number,
+      address,
+    },
+    {
+      where: {
+        id: id,
+      },
+      returning: true,
+    },
+  )
+  return result
+}
+module.exports = { getCompanyById, updateCompnyById }
