cad95f56d3b6c987b2b0d8d2464d0401a5230e45
Junatius <cjuniar0@gmail.com>
2023-11-13 00:05:18 +0800

    feat: add morganMiddleware and logger

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-const express = require('express')
-const { api, port } = require('./config')
-const swaggerUi = require('swagger-ui-express')
-const specs = require('./utils/swagger')
+const express = require('express');
+const { api, port } = require('./config');
+const logger = require('./utils/logger');
+const swaggerUi = require('swagger-ui-express');
+const specs = require('./utils/swagger');
+const { morganMiddleware } = require('./middlewares');
 
-const app = express()
+const app = express();
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs))
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use(morganMiddleware);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`)
-})
+  logger.info(`Server is running on port ${port}`);
+});
