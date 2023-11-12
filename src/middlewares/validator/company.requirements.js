c16af3e79699292a4b6e0e38e56990d2d278944c
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-13 05:44:24 +0700

    feat: add validator middleware

--- src/middlewares/validator/company.requirements.js
+++ src/middlewares/validator/company.requirements.js
@@ -1,30 +1,27 @@
-const { body, param } = require('express-validator')
+const { body, param } = require('express-validator');
 
 const requirements = {
   getCompanyById: [param('id').isInt({ min: 1 })],
 
   updateCompanyById: [
     param('id').isInt({ min: 1 }),
-    body('photo_profile').isString().isLength({ min: 1 }),
-    body('name').isString().isLength({ min: 3 }),
-    body('type')
-      .isString()
-      .isIn([
-        'Technology',
-        'Healthcare',
-        'Finance',
-        'Education',
-        'Retail',
-        'Entertainment',
-        'Manufacturing',
-        'Consulting',
-        'Energy',
-      ]),
-    body('email').isString().isLength({ min: 3 }),
-    body('website').isString(),
-    body('phone_number').isString(),
-    body('address').isString(),
-  ],
-}
+    body('name').isString()
+    // body('type').isIn([
+    //   'Technology',
+    //   'Healthcare',
+    //   'Finance',
+    //   'Education',
+    //   'Retail',
+    //   'Entertainment',
+    //   'Manufacturing',
+    //   'Consulting',
+    //   'Energy'
+    // ])
+    // body('email').isString().isLength({ min: 3 }),
+    // body('website').isString(),
+    // body('phone_number').isString(),
+    // body('address').isString(),
+  ]
+};
 
-module.exports = requirements
+module.exports = requirements;
