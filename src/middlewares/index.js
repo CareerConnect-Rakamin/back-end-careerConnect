153d97531c6c4caaca47c9fd39065b241882862c
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-13 08:41:22 +0700

    style: garis baru

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,4 +1,5 @@
 const authMiddlewares = require('./auth.middlewares');
 const validator = require('./validator');
 const uploadFileMiddleware = require('./multer.middlerware');
+
 module.exports = { authMiddlewares, validator, uploadFileMiddleware };
