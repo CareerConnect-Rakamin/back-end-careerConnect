b81f100e9963c1ee078b9e39fed8f32b047c5408
Inal Mahpud <inalmahpud1@gmail.com>
2023-11-13 07:39:36 +0700

    feat: add and fix validator for updateById

--- src/middlewares/validator/company.requirements.js
+++ src/middlewares/validator/company.requirements.js
@@ -5,22 +5,22 @@ const requirements = {
 
   updateCompanyById: [
     param('id').isInt({ min: 1 }),
-    body('name').isString()
-    // body('type').isIn([
-    //   'Technology',
-    //   'Healthcare',
-    //   'Finance',
-    //   'Education',
-    //   'Retail',
-    //   'Entertainment',
-    //   'Manufacturing',
-    //   'Consulting',
-    //   'Energy'
-    // ])
-    // body('email').isString().isLength({ min: 3 }),
-    // body('website').isString(),
-    // body('phone_number').isString(),
-    // body('address').isString(),
+    body('name').isString().isLength({ min: 3 }),
+    body('type').isIn([
+      'Technology',
+      'Healthcare',
+      'Finance',
+      'Education',
+      'Retail',
+      'Entertainment',
+      'Manufacturing',
+      'Consulting',
+      'Energy'
+    ]),
+    body('email').isString().isLength({ min: 3 }).contains('@'),
+    body('website').isString().contains('.'),
+    body('phone_number').isString().isLength({ min: 9 }),
+    body('address').isString().isLength({ min: 10 })
   ]
 };
 
