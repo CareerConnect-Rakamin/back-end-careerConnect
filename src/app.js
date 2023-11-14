5fb2d68c352848b0db183900b117832b31f30c58
erzaikhsan <ikhsanierza@gmail.com>
2023-11-14 15:21:35 +0700

    fix: fixing app.js and add server.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
-const express = require('express');
-const { api, port } = require('./config');
+const { port } = require('./config');
 const logger = require('./utils/logger');
-const swaggerUi = require('swagger-ui-express');
-const specs = require('./utils/swagger');
-const { morganMiddleware } = require('./middlewares');
-const routes = require('./routes');
-
-const app = express();
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
-app.use(morganMiddleware);
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(`/${api}`, routes);
+const app = require('./server');
 
 app.listen(port, () => {
   logger.info(`Server is running on port ${port}`);
 });
-
-module.exports = app;
