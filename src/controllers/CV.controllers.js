ffed7456c19fb465d2c033ebda09c6671abe9a61
Junatius <cjuniar0@gmail.com>
2023-11-23 03:30:42 +0800

    fix: adjust cv controller

--- src/controllers/CV.controllers.js
+++ src/controllers/CV.controllers.js
@@ -2,17 +2,15 @@ const { CVService } = require('../services');
 const logger = require('../utils/logger');
 
 const UpdateCV = async (req, res) => {
-  const id = req.params.id;
+  const id = req.userdata.id;
   const fileDir = req.file.path;
   const fileName = req.file.filename;
   try {
     const file = `public/uploads/${id}/${fileName}`;
-    const result = await CVService.UpdateCV({ id, cv_path: file });
-    if (result) {
-      res
-        .status(200)
-        .json({ status: 'Success', message: 'CV Uploaded successfully' });
-    }
+    await CVService.UpdateCV({ id, cv_path: file });
+    res
+      .status(200)
+      .json({ status: 'Success', message: 'CV Uploaded successfully' });
   } catch (error) {
     logger.error(error.message);
     res
@@ -22,14 +20,19 @@ const UpdateCV = async (req, res) => {
 };
 
 const DeleteCV = async (req, res) => {
-  const id = req.params.id;
+  const id = req.userdata.id;
   try {
     const result = await CVService.DeleteCV({ id });
     if (result) {
-      res.status(200).json({ message: 'CV deleted successfully...' });
+      res
+        .status(200)
+        .json({ status: 'Success', message: 'CV deleted successfully' });
     }
   } catch (error) {
-    res.status(500).send({ message: 'internal server error...', error });
+    logger.error(error.message);
+    res
+      .status(500)
+      .send({ status: 'failed', message: 'internal server error' });
   }
 };
 
